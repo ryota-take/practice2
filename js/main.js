bd67718310c3edd3fce4d237469ae023ac0bc049
ryota-take <77515214+ryota-take@users.noreply.github.com>
2021-01-17 07:28:11 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,594 @@
+'use strict';
+
+
+{
+  const fh1 = document.getElementById('fh1');
+  const down1 = document.getElementById('down1');
+  const fp_1 = document.querySelector('.fp_1');
+  const up1_c = document.querySelector('.up1_c');
+
+
+  down1.addEventListener('click', () => {
+  fp_1.classList.toggle('show');
+  up1_c.classList.toggle('show');
+  down1.classList.toggle('show');
+});
+
+
+up1_c.addEventListener('click', () => {
+  fp_1.classList.toggle('show');
+  up1_c.classList.toggle('show');
+  down1.classList.toggle('show');
+});
+fh1.addEventListener('click', () => {
+  fp_1.classList.toggle('show');
+  up1_c.classList.toggle('show');
+  down1.classList.toggle('show');
+
+});
+}
+  
+
+
+{
+  const fh2 = document.getElementById('fh2');
+  const down2 = document.getElementById('down2');
+  const fp_2 = document.querySelector('.fp_2');
+  const up2_c = document.querySelector('.up2_c');
+
+
+  down2.addEventListener('click', () => {
+  fp_2.classList.toggle('show');
+  up2_c.classList.toggle('show');
+  down2.classList.toggle('show');
+});
+
+
+up2_c.addEventListener('click', () => {
+  fp_2.classList.toggle('show');
+  up2_c.classList.toggle('show');
+  down2.classList.toggle('show');
+});
+fh2.addEventListener('click', () => {
+  fp_2.classList.toggle('show');
+  up2_c.classList.toggle('show');
+  down2.classList.toggle('show');
+
+});
+}
+  
+
+
+{
+  const fh3 = document.getElementById('fh3');
+  const down3 = document.getElementById('down3');
+  const fp_3 = document.querySelector('.fp_3');
+  const up3_c = document.querySelector('.up3_c');
+
+
+  down3.addEventListener('click', () => {
+  fp_3.classList.toggle('show');
+  up3_c.classList.toggle('show');
+  down3.classList.toggle('show');
+});
+
+
+up3_c.addEventListener('click', () => {
+  fp_3.classList.toggle('show');
+  up3_c.classList.toggle('show');
+  down3.classList.toggle('show');
+});
+fh3.addEventListener('click', () => {
+  fp_3.classList.toggle('show');
+  up3_c.classList.toggle('show');
+  down3.classList.toggle('show');
+
+});
+}
+  
+
+
+{
+  const fh4 = document.getElementById('fh4');
+  const down4 = document.getElementById('down4');
+  const fp_4 = document.querySelector('.fp_4');
+  const up4_c = document.querySelector('.up4_c');
+
+
+  down4.addEventListener('click', () => {
+  fp_4.classList.toggle('show');
+  up4_c.classList.toggle('show');
+  down4.classList.toggle('show');
+});
+
+
+up4_c.addEventListener('click', () => {
+  fp_4.classList.toggle('show');
+  up4_c.classList.toggle('show');
+  down4.classList.toggle('show');
+});
+fh4.addEventListener('click', () => {
+  fp_4.classList.toggle('show');
+  up4_c.classList.toggle('show');
+  down4.classList.toggle('show');
+
+});
+}
+
+
+{
+  const fh5 = document.getElementById('fh5');
+  const down5 = document.getElementById('down5');
+  const fp_5 = document.querySelector('.fp_5');
+  const up5_c = document.querySelector('.up5_c');
+
+
+  down5.addEventListener('click', () => {
+  fp_5.classList.toggle('show');
+  up5_c.classList.toggle('show');
+  down5.classList.toggle('show');
+});
+
+
+up5_c.addEventListener('click', () => {
+  fp_5.classList.toggle('show');
+  up5_c.classList.toggle('show');
+  down5.classList.toggle('show');
+});
+fh5.addEventListener('click', () => {
+  fp_5.classList.toggle('show');
+  up5_c.classList.toggle('show');
+  down5.classList.toggle('show');
+
+});
+}
+  
+
+
+
+
+
+// よくある質問
+
+
+{
+  const qa1_p = document.querySelector('.qa1_p');
+  const qa1_d_img = document.querySelector('.qa1_d_img');
+  const qa_ann1 = document.querySelector('.qa_ann1');
+  const qa1_u_img = document.querySelector('.qa1_u_img');
+
+
+  qa1_d_img.addEventListener('click', () => {
+  qa_ann1.classList.toggle('show2');
+  qa1_u_img.classList.toggle('show2');
+  qa1_d_img.classList.toggle('show2');
+});
+qa1_u_img.addEventListener('click', () => {
+  qa_ann1.classList.toggle('show2');
+  qa1_u_img.classList.toggle('show2');
+  qa1_d_img.classList.toggle('show2');
+});
+qa1_p.addEventListener('click', () => {
+  qa_ann1.classList.toggle('show2');
+  qa1_u_img.classList.toggle('show2');
+  qa1_d_img.classList.toggle('show2');
+
+});
+}
+
+
+{
+  const qa2_p = document.querySelector('.qa2_p');
+  const qa2_d_img = document.querySelector('.qa2_d_img');
+  const qa_ann2 = document.querySelector('.qa_ann2');
+  const qa2_u_img = document.querySelector('.qa2_u_img');
+
+
+  qa2_d_img.addEventListener('click', () => {
+  qa_ann2.classList.toggle('show2');
+  qa2_u_img.classList.toggle('show2');
+  qa2_d_img.classList.toggle('show2');
+});
+qa2_u_img.addEventListener('click', () => {
+  qa_ann2.classList.toggle('show2');
+  qa2_u_img.classList.toggle('show2');
+  qa2_d_img.classList.toggle('show2');
+});
+qa2_p.addEventListener('click', () => {
+  qa_ann2.classList.toggle('show2');
+  qa2_u_img.classList.toggle('show2');
+  qa2_d_img.classList.toggle('show2');
+
+});
+}
+
+{
+  const qa3_p = document.querySelector('.qa3_p');
+  const qa3_d_img = document.querySelector('.qa3_d_img');
+  const qa_ann3 = document.querySelector('.qa_ann3');
+  const qa3_u_img = document.querySelector('.qa3_u_img');
+
+
+  qa3_d_img.addEventListener('click', () => {
+  qa_ann3.classList.toggle('show2');
+  qa3_u_img.classList.toggle('show2');
+  qa3_d_img.classList.toggle('show2');
+});
+qa3_u_img.addEventListener('click', () => {
+  qa_ann3.classList.toggle('show2');
+  qa3_u_img.classList.toggle('show2');
+  qa3_d_img.classList.toggle('show2');
+});
+qa3_p.addEventListener('click', () => {
+  qa_ann3.classList.toggle('show2');
+  qa3_u_img.classList.toggle('show2');
+  qa3_d_img.classList.toggle('show2');
+
+});
+}
+
+
+
+
+{
+  const qa4_p = document.querySelector('.qa4_p');
+  const qa4_d_img = document.querySelector('.qa4_d_img');
+  const qa_ann4 = document.querySelector('.qa_ann4');
+  const qa4_u_img = document.querySelector('.qa4_u_img');
+
+
+  qa4_d_img.addEventListener('click', () => {
+  qa_ann4.classList.toggle('show2');
+  qa4_u_img.classList.toggle('show2');
+  qa4_d_img.classList.toggle('show2');
+});
+qa4_u_img.addEventListener('click', () => {
+  qa_ann4.classList.toggle('show2');
+  qa4_u_img.classList.toggle('show2');
+  qa4_d_img.classList.toggle('show2');
+});
+qa4_p.addEventListener('click', () => {
+  qa_ann4.classList.toggle('show2');
+  qa4_u_img.classList.toggle('show2');
+  qa4_d_img.classList.toggle('show2');
+
+});
+}
+
+
+
+{
+  const qa5_p = document.querySelector('.qa5_p');
+  const qa5_d_img = document.querySelector('.qa5_d_img');
+  const qa_ann5 = document.querySelector('.qa_ann5');
+  const qa5_u_img = document.querySelector('.qa5_u_img');
+
+
+  qa5_d_img.addEventListener('click', () => {
+  qa_ann5.classList.toggle('show2');
+  qa5_u_img.classList.toggle('show2');
+  qa5_d_img.classList.toggle('show2');
+});
+qa5_u_img.addEventListener('click', () => {
+  qa_ann5.classList.toggle('show2');
+  qa5_u_img.classList.toggle('show2');
+  qa5_d_img.classList.toggle('show2');
+});
+qa5_p.addEventListener('click', () => {
+  qa_ann5.classList.toggle('show2');
+  qa5_u_img.classList.toggle('show2');
+  qa5_d_img.classList.toggle('show2');
+
+});
+}
+
+
+
+{
+  const qa6_p = document.querySelector('.qa6_p');
+  const qa6_d_img = document.querySelector('.qa6_d_img');
+  const qa_ann6 = document.querySelector('.qa_ann6');
+  const qa6_u_img = document.querySelector('.qa6_u_img');
+
+
+  qa6_d_img.addEventListener('click', () => {
+  qa_ann6.classList.toggle('show2');
+  qa6_u_img.classList.toggle('show2');
+  qa6_d_img.classList.toggle('show2');
+});
+qa6_u_img.addEventListener('click', () => {
+  qa_ann6.classList.toggle('show2');
+  qa6_u_img.classList.toggle('show2');
+  qa6_d_img.classList.toggle('show2');
+});
+qa6_p.addEventListener('click', () => {
+  qa_ann6.classList.toggle('show2');
+  qa6_u_img.classList.toggle('show2');
+  qa6_d_img.classList.toggle('show2');
+
+});
+}
+
+
+
+{
+  const qa7_p = document.querySelector('.qa7_p');
+  const qa7_d_img = document.querySelector('.qa7_d_img');
+  const qa_ann7 = document.querySelector('.qa_ann7');
+  const qa7_u_img = document.querySelector('.qa7_u_img');
+
+
+  qa7_d_img.addEventListener('click', () => {
+  qa_ann7.classList.toggle('show2');
+  qa7_u_img.classList.toggle('show2');
+  qa7_d_img.classList.toggle('show2');
+});
+qa7_u_img.addEventListener('click', () => {
+  qa_ann7.classList.toggle('show2');
+  qa7_u_img.classList.toggle('show2');
+  qa7_d_img.classList.toggle('show2');
+});
+qa7_p.addEventListener('click', () => {
+  qa_ann7.classList.toggle('show2');
+  qa7_u_img.classList.toggle('show2');
+  qa7_d_img.classList.toggle('show2');
+
+});
+}
+
+
+
+{
+  const qa8_p = document.querySelector('.qa8_p');
+  const qa8_d_img = document.querySelector('.qa8_d_img');
+  const qa_ann8 = document.querySelector('.qa_ann8');
+  const qa8_u_img = document.querySelector('.qa8_u_img');
+
+
+  qa8_d_img.addEventListener('click', () => {
+  qa_ann8.classList.toggle('show2');
+  qa8_u_img.classList.toggle('show2');
+  qa8_d_img.classList.toggle('show2');
+});
+qa8_u_img.addEventListener('click', () => {
+  qa_ann8.classList.toggle('show2');
+  qa8_u_img.classList.toggle('show2');
+  qa8_d_img.classList.toggle('show2');
+});
+qa8_p.addEventListener('click', () => {
+  qa_ann8.classList.toggle('show2');
+  qa8_u_img.classList.toggle('show2');
+  qa8_d_img.classList.toggle('show2');
+
+});
+}
+
+
+
+{
+  const qa9_p = document.querySelector('.qa9_p');
+  const qa9_d_img = document.querySelector('.qa9_d_img');
+  const qa_ann9 = document.querySelector('.qa_ann9');
+  const qa9_u_img = document.querySelector('.qa9_u_img');
+
+
+  qa9_d_img.addEventListener('click', () => {
+  qa_ann9.classList.toggle('show2');
+  qa9_u_img.classList.toggle('show2');
+  qa9_d_img.classList.toggle('show2');
+});
+qa9_u_img.addEventListener('click', () => {
+  qa_ann9.classList.toggle('show2');
+  qa9_u_img.classList.toggle('show2');
+  qa9_d_img.classList.toggle('show2');
+});
+qa9_p.addEventListener('click', () => {
+  qa_ann9.classList.toggle('show2');
+  qa9_u_img.classList.toggle('show2');
+  qa9_d_img.classList.toggle('show2');
+
+});
+}
+
+
+
+{
+  const qa10_p = document.querySelector('.qa10_p');
+  const qa10_d_img = document.querySelector('.qa10_d_img');
+  const qa_ann10 = document.querySelector('.qa_ann10');
+  const qa10_u_img = document.querySelector('.qa10_u_img');
+
+
+  qa10_d_img.addEventListener('click', () => {
+  qa_ann10.classList.toggle('show2');
+  qa10_u_img.classList.toggle('show2');
+  qa10_d_img.classList.toggle('show2');
+});
+qa10_u_img.addEventListener('click', () => {
+  qa_ann10.classList.toggle('show2');
+  qa10_u_img.classList.toggle('show2');
+  qa10_d_img.classList.toggle('show2');
+});
+qa10_p.addEventListener('click', () => {
+  qa_ann10.classList.toggle('show2');
+  qa10_u_img.classList.toggle('show2');
+  qa10_d_img.classList.toggle('show2');
+
+});
+}
+
+
+
+{
+  const qa11_p = document.querySelector('.qa11_p');
+  const qa11_d_img = document.querySelector('.qa11_d_img');
+  const qa_ann11 = document.querySelector('.qa_ann11');
+  const qa11_u_img = document.querySelector('.qa11_u_img');
+
+
+  qa11_d_img.addEventListener('click', () => {
+  qa_ann11.classList.toggle('show2');
+  qa11_u_img.classList.toggle('show2');
+  qa11_d_img.classList.toggle('show2');
+});
+qa11_u_img.addEventListener('click', () => {
+  qa_ann11.classList.toggle('show2');
+  qa11_u_img.classList.toggle('show2');
+  qa11_d_img.classList.toggle('show2');
+});
+qa11_p.addEventListener('click', () => {
+  qa_ann11.classList.toggle('show2');
+  qa11_u_img.classList.toggle('show2');
+  qa11_d_img.classList.toggle('show2');
+
+});
+}
+
+
+
+{
+  const qa12_p = document.querySelector('.qa12_p');
+  const qa12_d_img = document.querySelector('.qa12_d_img');
+  const qa_ann12 = document.querySelector('.qa_ann12');
+  const qa12_u_img = document.querySelector('.qa12_u_img');
+
+
+  qa12_d_img.addEventListener('click', () => {
+  qa_ann12.classList.toggle('show2');
+  qa12_u_img.classList.toggle('show2');
+  qa12_d_img.classList.toggle('show2');
+});
+qa12_u_img.addEventListener('click', () => {
+  qa_ann12.classList.toggle('show2');
+  qa12_u_img.classList.toggle('show2');
+  qa12_d_img.classList.toggle('show2');
+});
+qa12_p.addEventListener('click', () => {
+  qa_ann12.classList.toggle('show2');
+  qa12_u_img.classList.toggle('show2');
+  qa12_d_img.classList.toggle('show2');
+
+});
+}
+
+
+
+{
+  const qa13_p = document.querySelector('.qa13_p');
+  const qa13_d_img = document.querySelector('.qa13_d_img');
+  const qa_ann13 = document.querySelector('.qa_ann13');
+  const qa13_u_img = document.querySelector('.qa13_u_img');
+
+
+  qa13_d_img.addEventListener('click', () => {
+  qa_ann13.classList.toggle('show2');
+  qa13_u_img.classList.toggle('show2');
+  qa13_d_img.classList.toggle('show2');
+});
+qa13_u_img.addEventListener('click', () => {
+  qa_ann13.classList.toggle('show2');
+  qa13_u_img.classList.toggle('show2');
+  qa13_d_img.classList.toggle('show2');
+});
+qa13_p.addEventListener('click', () => {
+  qa_ann13.classList.toggle('show2');
+  qa13_u_img.classList.toggle('show2');
+  qa13_d_img.classList.toggle('show2');
+
+});
+}
+
+
+
+{
+  const qa14_p = document.querySelector('.qa14_p');
+  const qa14_d_img = document.querySelector('.qa14_d_img');
+  const qa_ann14 = document.querySelector('.qa_ann14');
+  const qa14_u_img = document.querySelector('.qa14_u_img');
+
+
+  qa14_d_img.addEventListener('click', () => {
+  qa_ann14.classList.toggle('show2');
+  qa14_u_img.classList.toggle('show2');
+  qa14_d_img.classList.toggle('show2');
+});
+qa14_u_img.addEventListener('click', () => {
+  qa_ann14.classList.toggle('show2');
+  qa14_u_img.classList.toggle('show2');
+  qa14_d_img.classList.toggle('show2');
+});
+qa14_p.addEventListener('click', () => {
+  qa_ann14.classList.toggle('show2');
+  qa14_u_img.classList.toggle('show2');
+  qa14_d_img.classList.toggle('show2');
+
+});
+}
+
+
+
+{
+  const qa15_p = document.querySelector('.qa15_p');
+  const qa15_d_img = document.querySelector('.qa15_d_img');
+  const qa_ann15 = document.querySelector('.qa_ann15');
+  const qa15_u_img = document.querySelector('.qa15_u_img');
+
+
+  qa15_d_img.addEventListener('click', () => {
+  qa_ann15.classList.toggle('show2');
+  qa15_u_img.classList.toggle('show2');
+  qa15_d_img.classList.toggle('show2');
+});
+qa15_u_img.addEventListener('click', () => {
+  qa_ann15.classList.toggle('show2');
+  qa15_u_img.classList.toggle('show2');
+  qa15_d_img.classList.toggle('show2');
+});
+qa15_p.addEventListener('click', () => {
+  qa_ann15.classList.toggle('show2');
+  qa15_u_img.classList.toggle('show2');
+  qa15_d_img.classList.toggle('show2');
+
+});
+}
+
+
+
+{
+  const qa16_p = document.querySelector('.qa16_p');
+  const qa16_d_img = document.querySelector('.qa16_d_img');
+  const qa_ann16 = document.querySelector('.qa_ann16');
+  const qa16_u_img = document.querySelector('.qa16_u_img');
+
+
+  qa16_d_img.addEventListener('click', () => {
+  qa_ann16.classList.toggle('show2');
+  qa16_u_img.classList.toggle('show2');
+  qa16_d_img.classList.toggle('show2');
+});
+qa16_u_img.addEventListener('click', () => {
+  qa_ann16.classList.toggle('show2');
+  qa16_u_img.classList.toggle('show2');
+  qa16_d_img.classList.toggle('show2');
+});
+qa16_p.addEventListener('click', () => {
+  qa_ann16.classList.toggle('show2');
+  qa16_u_img.classList.toggle('show2');
+  qa16_d_img.classList.toggle('show2');
+
+});
+}
+
+
+const flowWidth = document.querySelector('.mem-img1').offsetWidth;
+const flowHeight = document.querySelector('.mem-img1').offsetHeight;
+
+const take = flowWidth * flowHeight
+
+
+
+
+console.log(flowHeight) ;
+
+
+
